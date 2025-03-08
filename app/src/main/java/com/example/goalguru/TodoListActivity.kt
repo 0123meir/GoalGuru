37b02f8dc354bc17c08c0d183b3da7e0395ca075
Liraz <liraz581cohen@gmail.com>
2025-03-08 14:51:12 +0200

    created Todo List files and header in XML

---
+++ app/src/main/java/com/example/goalguru/TodoListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.goalguru
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TodoListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.todo_list)
+    }
+}
