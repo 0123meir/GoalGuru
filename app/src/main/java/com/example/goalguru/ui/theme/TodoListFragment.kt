423e071441816c22170514949d6c01ffea793b42
Liraz <liraz581cohen@gmail.com>
2025-03-15 14:49:41 +0200

    improve appearance of TodoList to look better and match with forum page

--- app/src/main/java/com/example/goalguru/ui/theme/TodoListFragment.kt
+++ app/src/main/java/com/example/goalguru/ui/theme/TodoListFragment.kt
@@ -4,17 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.goalguru.R
 import com.example.goalguru.TasksAdapter
 import com.example.goalguru.databinding.NewGoalLayoutBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoListFragment : Fragment() {
     private var listView: ListView? = null
-    private var newTask: Button? = null
+    private var newTask: FloatingActionButton? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
