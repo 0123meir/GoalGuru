56b0ba1fb6458688f5d5024b0fa95db9fc80349e
Meir <0123meir@gmail.com>
2025-03-15 21:26:10 +0200

    implemented scroll to top after creating new post

--- app/src/main/java/com/example/goalguru/ui/theme/ForumFragment.kt
+++ app/src/main/java/com/example/goalguru/ui/theme/ForumFragment.kt
@@ -95,6 +95,10 @@ class ForumFragment : Fragment(), PostDialogHandler.PostDialogCallback {
             val yourPostsFragment = findPostsFragment("your_posts")
             yourPostsFragment?.addNewPost(text, imageUrls)
 
+            //scroll up after creating a post
+            yourPostsFragment?.scrollToTop()
+
+
             // Show success message
             Toast.makeText(context, "Post created successfully!", Toast.LENGTH_SHORT).show()
 
